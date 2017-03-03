5d4baa07ab371299cfef4701ea0feb0bb319acfb
Andrii Bilousko <arestarh@ukr.net>
2017-03-03 10:43:10 +0200

    Update Test-IsNonInteractiveShell.ps1
    Script was tested with PSScriptAnalyzer and recommendations were implemented.
    Typos.

--- Scripts/Utilities/Test-IsNonInteractiveShell.ps1
+++ Scripts/Utilities/Test-IsNonInteractiveShell.ps1
@@ -1,17 +1,18 @@
+function Test-IsNonInteractiveShell {
 <#
 .SYNOPSIS
 Returns boolean determining if prompt was run noninteractive.
 .DESCRIPTION
-First, we check `[Environment]::UserInteractive` to determine if we're if the shell if running 
+First, we check [Environment]::UserInteractive to determine if we're if the shell if running 
 interactively. An example of not running interactively would be if the shell is running as a service.
 
-If we are running interactively, we check the Command Line Arguments to see if the `-NonInteractive` 
+If we are running interactively, we check the Command Line Arguments to see if the '-NonInteractive'
 switch was used; or an abbreviation of the switch.
 .LINK
-https://github.com/Vertigion/Test-IsNonInteractiveShell
+https://github.com/UNT-CAS-ITS/Test-IsNonInteractiveShell/blob/master/Test-IsNonInteractiveShell.ps1
 #>
-function Test-IsNonInteractiveShell {
-    if ([Environment]::UserInteractive -and (-not ([Environment]::GetCommandLineArgs() | ?{ $_ -like '-NonI*' }))) {
+
+    if ([Environment]::UserInteractive -and (-not ([Environment]::GetCommandLineArgs() | Where-Object -FilterScript { $_ -like '-NonI*' }))) {
         # Test each Arg for match of abbreviated '-NonInteractive' command.
         return $true
     }
